3fda668e3072d0bb45e5bfb249501fde6c0d4382
neulii <neulii@gmx.at>
2020-05-24 11:04:59 +0200

    added system look and feel

--- src/jpoints/JPoints.java
+++ src/jpoints/JPoints.java
@@ -1,10 +1,29 @@
 package jpoints;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class JPoints {
 
     private static MainWindow mainWindow;
 
     static public void main(String args[])
     {
+    	
+    	//set look and feel 
+    	try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+    	
+    	
         mainWindow = new MainWindow(700,300);
         mainWindow.show();
     }
