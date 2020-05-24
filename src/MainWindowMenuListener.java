7a60fdd7afb0f682c44369f1f85ac57ae6f78ff1
neulii <neulii@gmx.at>
2020-05-24 09:11:53 +0200

    add todos

--- src/MainWindowMenuListener.java
+++ src/MainWindowMenuListener.java
@@ -14,15 +14,20 @@ public class MainWindowMenuListener implements ActionListener {
         switch (text)
         {
             case "Neues Spiel":
+            //TODO new game listener
                 JOptionPane.showMessageDialog(null,"neu");
                 break;
 
+
             case "Beenden":
+            //TODO beenden listener
+
                 JOptionPane.showMessageDialog(null,"end");
 
                 break;
 
             case "Info":
+            //TODO info listener
                 JOptionPane.showMessageDialog(null,"info");
 
                 break;
