774d3fd3c2b11976dcb81bf82223bd5cea4765a3
neulii <neulii@gmx.at>
2020-05-24 18:48:39 +0200

    label color is now editable with a function

--- src/jpoints/PlayerInputPanel.java
+++ src/jpoints/PlayerInputPanel.java
@@ -11,31 +11,36 @@ public class PlayerInputPanel extends JPanel {
 	private JTextField inputName;
 	private JLabel playerNameLabel;
 
-	public PlayerInputPanel(int width, int height, String labelText, float textSize) {
+	public PlayerInputPanel(int width_label, int width_InputField, int height, String labelText, float textSize) {
 	
 		setLayout(null);
 		
-		this.setSize(width, height);
+		this.setSize(width_label + width_InputField , height);
 		
-		this.setBackground(Color.red);
+		//this.setBackground(Color.red);
 		
 		playerNameLabel = new JLabel(labelText);
 		
 		playerNameLabel.setLocation(0, 0);
 		playerNameLabel.setOpaque(true);
-		playerNameLabel.setBackground(Color.blue);
+		//playerNameLabel.setBackground(Color.blue);
 		playerNameLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		playerNameLabel.setSize((int)(width)/3,height);
+		playerNameLabel.setSize(width_label,height);
 	
 		playerNameLabel.setFont(playerNameLabel.getFont().deriveFont(textSize));
 		add(playerNameLabel);
 		
 		inputName = new JTextField();
-		inputName.setLocation(width/3, 0);
-		inputName.setSize(width-playerNameLabel.getWidth(),height);
+		inputName.setLocation(width_label, 0);
+		inputName.setSize(width_InputField,height);
 		inputName.setHorizontalAlignment(SwingConstants.CENTER);
 		inputName.setFont(playerNameLabel.getFont().deriveFont(textSize));
 		add(inputName);
+	}
+	
+	public void setLabelBackground(Color color)
+	{
+		playerNameLabel.setBackground(color);
 	}
 }
