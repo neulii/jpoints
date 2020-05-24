b7387a9e4ca88b2dbccb5a8737d551effea069fc
neulii <neulii@gmx.at>
2020-05-24 18:49:31 +0200

    added ok button and frame is now a dialog -> modality

--- src/jpoints/NewGameWindow.java
+++ src/jpoints/NewGameWindow.java
@@ -1,31 +1,55 @@
 package jpoints;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Color;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 
-public class NewGameWindow extends JFrame {
+public class NewGameWindow {
 
-	private PlayerInputPanel pip;
+	private PlayerInputPanel playerOne;
+	private PlayerInputPanel playerTwo;
 	
+	private JDialog window;
 	
-	public NewGameWindow() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		setLayout(null);
+	public NewGameWindow(JFrame parentWindow) {
 		
-		pip = new PlayerInputPanel(400,50,"stefan",30);
+		window = new JDialog(parentWindow, "Neues Spiel erstellen", JDialog.ModalityType.DOCUMENT_MODAL);
+		window.setBounds(100, 100, 450, 300);
+		window.getContentPane().setLayout(null);
 		
-		pip.setLocation(0,0);
+		window.setLocationRelativeTo(parentWindow);
 		
-		this.add(pip);
+		window.setResizable(false);
+		window.setModalExclusionType(null);
 		
 		
+		JLabel creatGameLabel = new JLabel("Spieler erstellen:");
+		creatGameLabel.setBounds(30, 20, 216, 16);
+		window.getContentPane().add(creatGameLabel);
 		
+		playerOne = new PlayerInputPanel(150,250,50,"spieler 1:",30);
+		playerOne.setLocation(30,50);
+		playerOne.setLabelBackground(Color.yellow);
+		
+		playerTwo = new PlayerInputPanel(150,250,50,"spieler 2:",30);
+		playerTwo.setLocation(30, 130);
+		playerTwo.setLabelBackground(Color.green);
+		
+		
+		window.getContentPane().add(playerOne);
+		window.getContentPane().add(playerTwo);
+		
+		JButton okButton = new JButton("OK");
+		okButton.setBounds(156, 204, 138, 52);
+		window.getContentPane().add(okButton);
+	}
+	
+	public void showModal()
+	{
+		window.setVisible(true);
 		
 	}
-
 }
