8b8b71ac853948598c998899d8ab3f4e888e49ab
neulii <neulii@gmx.at>
2020-06-16 19:26:13 +0200

    added layouting mainPanel and jtable

--- src/jpoints/MainGamePanel.java
+++ src/jpoints/MainGamePanel.java
@@ -1,30 +1,81 @@
 package jpoints;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Point;
+
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class MainGamePanel extends JPanel {
 
 	
 	private static final long serialVersionUID = -8749292918188412931L;
-
+	
 	Game game;
 	PlayerInputPanel pOnePoints;
 	PlayerInputPanel pTwoPoints;
+	JTable table;
+
+	JPanel leftPanel;
+	JPanel rightPanel;
 	
 	//TODO mainpanel gehoert noch gemacht
 	public MainGamePanel(Game game)
 	{
 		this.game = game;
-		setLayout(null);
+		setLayout(new GridLayout(1, 2));
+		
+		//Leftpanel
+		leftPanel = new JPanel();
+		//leftPanel.setBackground(Color.red);
+		leftPanel.setLayout(null);
+		
+		
+		
+		//rightpanel
+		rightPanel = new JPanel();
+		//rightPanel.setBackground(Color.blue);
+		
+		
+		this.add(leftPanel);
+		this.add(rightPanel);
+		
+		
 		
 		pOnePoints = new PlayerInputPanel(100, 200, 40, game.getPlayers().elementAt(0).getName(), 20);
 		pOnePoints.setLocation(20, 50);
-		this.add(pOnePoints);
-		
+		leftPanel.add(pOnePoints);
 		
 		pTwoPoints = new PlayerInputPanel(100, 200, 40, game.getPlayers().elementAt(1).getName(), 20);
 		pTwoPoints.setLocation(20, 120);
-		this.add(pTwoPoints);
+		leftPanel.add(pTwoPoints);
 		
+		//headers for the table
+        String[] columns = new String[] {
+            "Id", "Name", "Hourly Rate", "Part Time"
+        };
+         
+        //actual data for the table in a 2d array
+        Object[][] data = new Object[][] {
+            {1, "John", 40.0, false },
+            {2, "Rambo", 70.0, false },
+            {3, "Zorro", 60.0, true },
+        };
+        //create table with data
+        table = new JTable(data, columns);
+        table.setBounds(100, 200, 200, 100);
+         
+        JScrollPane pane = new JScrollPane(table);
+        
+      
+        //add the table to the frame
+      rightPanel.add(pane);
+       
+        
+       // this.revalidate();
+        
 	}
 }
